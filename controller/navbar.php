8f5792a0390cfd3bf4f5b64444715b4e8e540be6
HeisenbergY <33573384+HeisenbergY@users.noreply.github.com>
2017-11-12 16:50:16 +0800

    导航栏框架完成
    导航栏已经可以在首页显示

--- controller/navbar.php
+++ controller/navbar.php
@@ -1,6 +1,21 @@
-<?php include("../model/navbar.php"); ?>
+<?php include("model/navbar.php"); ?>
 <?php
 function nav_list(){
-	return get_nav();
+	$navList=get_nav();
+	$arr=array();
+	$navs=array();
+	for($i=0;$i<count($navList);$i++){
+		if($navList[$i]['pid']==0){
+			array_push($arr,$navList[$i]);
+			for($j=0;$j<count($navList);$j++){
+				if($navList[$i]['id']==$navList[$j]['pid']){
+					array_push($arr,$navList[$j]);
+				}
+			}
+			array_push($navs,$arr);
+		}
+		$arr=array();
+	}
+	return $navs;
 }
 ?>
--- view/navbar.php
+++ view/navbar.php
@@ -1,17 +1,36 @@
 <?php
-include('../controller/navbar.php');
+include('controller/navbar.php');
 $navList=nav_list();
-$arr=array();
-$navs=array();
-for($i=0;$i<count($navList);$i++){
-	array_push($arr,$navList[$i]);
-	for($j=0;$j<count($navList);$j++){
-		if($navList[$i]['id']==$navList[$j]['pid']){
-			array_push($arr,$navList[$j]);
-		}
-	}
-	array_push($navs,$arr);
-	$arr=array();
-}
-var_dump($navs);
 ?>
+<style>
+
+</style>
+<nav class="nav nav-tabs">
+	<div class="container-fluid">
+		<!--<div class="navbar-header">
+			<a class="navbar-brand" href="#">菜鸟教程</a>
+		</div>-->
+		<div>
+			<ul class="nav nav-tabs">
+				<?php for($i=0;$i<count($navList);$i++){ 
+					if(count($navList[$i])<=1){ ?>
+					<li <?php $i==0?print "class='active'":"" ?>>
+						<a href="<?php echo $navList[$i][0]['href'] ?>"><?php print $navList[$i][0]['name'] ?></a>
+					</li>
+					<?php }else{ ?>
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown">
+							<?php echo $navList[$i][0]["name"] ?>
+							<b class="caret"></b>
+						</a>
+						<ul class="dropdown-menu">
+							<?php for($j=1;$j<count($navList[$i]);$j++){?>
+							<li><a href="<?php echo $navList[$i][$j]['href'] ?>"><?php echo $navList[$i][$j]['name'] ?></a></li>
+							<?php } ?>
+						</ul>
+					</li>
+					<?php }
+				} ?>
+			</div>
+		</div>
+	</nav>
