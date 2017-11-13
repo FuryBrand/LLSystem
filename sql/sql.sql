bb59d45b7290d883d939129b1bb8255a6d40115d
FuryBrand <bindthemall@outlook.jp>
2017-11-13 12:50:54 +0800

    更新sql.sql文件

--- sql/sql.sql
+++ sql/sql.sql
@@ -1,17 +1,30 @@
-DROP database IF EXISTS llsystem;
-CREATE database llsystem DEFAULT CHARACTER SET 'utf8';
-use llsystem;
+DROP database IF EXISTS sjae;
+CREATE database sjae DEFAULT CHARACTER SET 'utf8';
+use sjae;
 -- ----------------------------
--- Table structure for navi
+-- Table structure for navbar
 -- ----------------------------
-DROP TABLE IF EXISTS `navi`;
-CREATE TABLE `navi` (
+DROP TABLE IF EXISTS `navbar`;
+CREATE TABLE IF NOT EXISTS `navbar` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `title` varchar(50) DEFAULT NULL COMMENT '标题',
-  `father_id` int(11) DEFAULT NULL COMMENT '父标题的id',
-  `url` varchar(100) DEFAULT NULL COMMENT '关联的链接',
+  `name` varchar(100) NOT NULL DEFAULT '0' COMMENT '导航栏目名称',
+  `group` int(11) NOT NULL DEFAULT '0' COMMENT '导航栏目分组',
+  `pid` int(11) NOT NULL DEFAULT '0' COMMENT '设置父id',
+  `href` text COMMENT '导航栏链接',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 COMMENT='导航栏';
+-- ----------------------------
+-- Table data for navbar
+-- ----------------------------
+INSERT INTO `navbar` (`id`, `name`, `group`, `pid`, `href`) VALUES
+	(1, '首页', 0, 0, NULL),
+	(2, '关于盛达杰森', 1, 0, NULL),
+	(3, '企业概况', 0, 2, NULL),
+	(4, '价值理念', 0, 2, NULL),
+	(5, '发展历程', 0, 2, NULL),
+	(6, '产品&服务', 0, 0, NULL),
+	(7, '半导体装备', 0, 6, NULL),
+	(8, '真空装备', 0, 6, NULL);
 -- ----------------------------
 -- Table structure for slideshow
 -- ----------------------------
