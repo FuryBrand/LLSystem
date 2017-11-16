db1cf94b56a1ab8915f8b72754822c944e7864a0
HeisenbergY <33573384+HeisenbergY@users.noreply.github.com>
2017-11-16 20:15:22 +0800

    找回被revert的方法
    找回了get_all_produces

--- model/produces.php
+++ model/produces.php
@@ -19,34 +19,11 @@ function get_produces_by_id($id){
     return db_select("*","produces","id",id);
 }
 
+//ly:映射表
+function get_all_produces(){
+    return db_select_all("*","produces");
+}
 ////////针对单一问题的特定方法///////
-//lwx:确认该id是否可以被删除
-function can_del($id){
-    //判断指定id是否为产品
-    $sql1 = "SELECT * FROM produces WHERE id=$id AND type=2";
-    $sql2 = "SELECT * FROM produces WHERE father_id = $id";
-    if(count(run_sql($sql1,true))==0){
-        return true;
-    } else if(count(run_sql($sql2,true))==0){
-        return true;
-    } else {
-        return false;
-    }
-}
-//lwx:获取最新的6条新闻
-function get_lastest_news(){
-    $sql="SELECT * FROM news ORDER BY create_date DESC LIMIT 6";
-    return run_sql($sql,true);
-}
-//lwx:返回新闻数量
-function get_news_counts(){
-    $sql="SELECT COUNT(*) FROM news";
-    return run_sql($sql,true);
-}
-//lwx:分页查询
-function get_paged_news($startIndex,$pageSize){
-    $sql="SELECT * FROM news ORDER BY create_date DESC LIMIT $startIndex,$pageSize";
-    return run_sql($sql,true);
-}
+
 ?>
 
