e9df6280f44c3e9ca5b61dc2ca559a7a83c9b18e
HeisenbergY <33573384+HeisenbergY@users.noreply.github.com>
2017-11-20 20:50:14 +0800

    完成admin新闻修改页面

--- model/news.php
+++ model/news.php
@@ -11,9 +11,9 @@ function del_news($id){
 	return db_delete("news",$id);
 }
 //lwx:改
-function update_news($id,$title,$type,$thumb){
-	$sql="UPDATE news SET type='".$type."', title='".$title."' ,thumb='".$thumb."' WHERE id='".$id."'";
-	return run_sql($sql,true);
+function update_news($id,$title,$type){
+	$sql="UPDATE news SET type='".$type."', title='".$title."' WHERE id='".$id."'";
+	return run_sql($sql);
 }
 //lwx:改一个字段
 function update_onepro_news($setField,$setVal,$id){
@@ -21,7 +21,7 @@ function update_onepro_news($setField,$setVal,$id){
 }
 //lwx:返回指定新闻页面
 function get_news_by_id($id){
-	$sql = "SELECT n.id,n.title,n.content,n.create_date,n.type,f.name FROM news n LEFT JOIN fk_news_type f ON n.type=f.id WHERE n.id=$id";
+	$sql = "SELECT n.id,n.title,n.content,n.thumb,n.create_date,n.type,f.name FROM news n LEFT JOIN fk_news_type f ON n.type=f.id WHERE n.id=$id";
 	return run_sql($sql,true);
 }
 //lwx:返回指定类型的新闻页面
