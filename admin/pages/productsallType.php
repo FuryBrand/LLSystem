acbaf96c3d72dd94bd709e78ae4f4dcb6bb6847c
FuryBrand <bindthemall@outlook.jp>
2017-11-21 10:36:37 +0800

    后台-产品管理-列表done

--- admin/pages/productsallType.php
+++ admin/pages/productsallType.php
@@ -16,7 +16,7 @@
 			</select>
 		</div>
 		<div class='col-lg-3'>
-			<select id="series" multiple="multiple" size="<?php echo count($company);?>">
+			<select id="series" multiple="multiple" size="<?php echo count($company);?>" onclick="selectSeries()">
 			<option>请先选择公司</option>
 			</select>
 		</div>
@@ -37,19 +37,94 @@
 		</div>
 		<div class='col-lg-3'>
 			<div>
-				<input type="button" class="btn btn-danger" value="删除所选系列" onclick="delCompany()"/>
+				<input type="button" class="btn btn-danger" value="删除所选系列" onclick="delSeries()"/>
 			</div>
 			<div>
-				<input type="text" style="width:70%;height:34px" id="addCompany" placeholder="请输入新的系列">
-				<input type="button" class="btn btn-info" value="添加" onclick="newCompany()"/>
+				<input type="text" style="width:70%;height:34px" id="addSeries" placeholder="请输入新的系列">
+				<input type="button" class="btn btn-info" value="添加" onclick="newSeries()"/>
 			</div>
 			<div>
-				<input type="text" style="width:70%;height:34px" id="editCompany" placeholder="修改系列名称">
-				<input type="button" class="btn btn-info" value="修改" onclick="updateCompany()"/>
+				<input type="text" style="width:70%;height:34px" id="editSeries" placeholder="修改系列名称">
+				<input type="button" class="btn btn-info" value="修改" onclick="updateSeries()"/>
 			</div>
 		</div>
 	</div>
 	<script type="text/javascript">
+		function selectSeries(){
+			$("#editSeries").val($("#series").find("option:selected").text());
+		}
+		function updateSeries(){
+			var editSeries = $("#editSeries").val();
+			var seriesid = $("#series").val()[0];
+			if(seriesid){
+				$.ajax({
+					type:"POST",
+					url:"../controller/ajax.php?fun=edit_series",
+					dataType:'json',
+					data:{seriesid:seriesid,editSeries:editSeries}
+				})
+				.done(function(data){
+					if("succ" in data){
+						window.location.reload();
+						//$("#addSeries").attr('placeholder',"请输入新的系列");
+					}
+				})
+				.fail(function (jqXHR, textStatus) {
+				alert("更新失败!")
+				console.log(jqXHR);
+				});				
+			}else{
+				alert("请先选择产品系列");
+			}
+		}
+		function newSeries(){
+			var companyid = $("#company").val();
+			var seriesval = $("#addSeries").val();
+			if(companyid){
+				$.ajax({
+					type:"POST",
+					url:"../controller/ajax.php?fun=new_series",
+					dataType:'json',
+					data:{seriesval:seriesval,pid:companyid[0]}
+				})
+				.done(function(data){
+					if("succ" in data){
+						window.location.reload();
+						//$("#addSeries").attr('placeholder',"请输入新的系列");
+					}
+				})
+				.fail(function (jqXHR, textStatus) {
+				alert("更新失败!")
+				console.log(jqXHR);
+				});
+			}else{
+				alert("请先选择公司");
+			}
+		}
+		function delSeries(){
+			var seriesid = $("#series").val();
+			if(seriesid){
+				$.ajax({
+					type:"POST",
+					url:"../controller/ajax.php?fun=del_series",
+					dataType:'json',
+					data:{id:seriesid[0]}
+				})
+				.done(function(data){
+					if("succ" in data){
+						window.location.reload();
+					}else if("cannotdel" in data){
+						alert('该系列含有产品，不能删除!');
+					}
+				})
+				.fail(function (jqXHR, textStatus) {
+				alert("更新失败!")
+				console.log(jqXHR);
+				});
+			}else{
+				alert("没有选择任何产品系列");
+			}
+		}
 		function updateCompany() {
 			var companyid = $("#company").val();
 			var title = $("#editCompany").val();
@@ -78,7 +153,7 @@
 			}).done(function (data) {
 				if(data.succ){
 					window.location.reload();
-					$("#addCompany").val("请输入新的公司");
+					$("#addCompany").attr("placeholder","请输入新的公司");
 				}
 			}).fail(function (jqXHR, textStatus) {
 				alert("添加失败!")
