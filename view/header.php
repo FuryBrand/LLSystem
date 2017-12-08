2252592a8f86fc406b22f43386945afcdb1f7cdd
HeisenbergY <33573384+HeisenbergY@users.noreply.github.com>
2017-12-08 17:04:34 +0800

    完成header响应式

--- view/header.php
+++ view/header.php
@@ -18,29 +18,34 @@
 		$from=$_GET["from"];
 	}
 	?>
+	<style type="text/css">
+	.logo-height{
+        	height:88px;
+        }
+	@media (max-width: 769px) {
+        .right-text{
+        	text-align: left;
+        }
+        .logo-height{
+        	height:auto;
+        }
+    }
+	</style>
 	<div class="row margin-top30">
-		<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-			<img src="./view/imgs/newlogo.png" style="height:74px">
+		<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+			<img src="./view/imgs/newlogo.png" class="logo-height">
 		</div>
-		<div class="col-lg-6 col-md-5 col-sm-6 col-xs-12 margin-top10">
-			<div>
-				<form method="post" action="./controller/ajax.php?fun=search">
-					<div style="float:right">
-						<div class="search">
-							<input type="radio" name="type" value="0" id="product" <?php ($hasKeyword&&$from=="0"||!$hasKeyword)?print 'checked' : "" ?>>
-							<label for="product" >产品</label>
-							<input type="radio" name="type" value="1" id="news" <?php ($hasKeyword&&$from=="1")?print 'checked' : "" ?>>
-							<label for="news">新闻</label>
-							<input type="text" name="keyword" value="<?php ($hasKeyword)?print $keyword : '' ?>" placeholder="关键字" style="width:40%;height:25px">
-							<input type="submit" value="搜索" class="btn btn-info" style="width:6 0px;height:26px;line-height:10px;vertical-align: top;">
-						</div>
-					</div>
-				</form>
-			</div>
-			<div>
-				<?php 
-				include_once(Root_Path.'/view/navbar.php') 
-				?>
-			</div>
+		<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+			<form method="post" class="search right-text" action="./controller/ajax.php?fun=search">
+				<input type="radio" name="type" value="0" id="product" <?php ($hasKeyword&&$from=="0"||!$hasKeyword)?print 'checked' : "" ?>>
+				<label for="product" style="margin-right:10px;">产品</label>
+				<input type="radio" name="type" value="1" id="news" <?php ($hasKeyword&&$from=="1")?print 'checked' : "" ?>>
+				<label for="news" style="margin-right:20px;">新闻</label>
+				<input type="text" name="keyword" value="<?php ($hasKeyword)?print $keyword : '' ?>" placeholder="关键字" style="width:40%;height:30px">
+				<input type="submit" value="搜索" class="btn btn-info" style="width:60px;height:30px;line-height:10px;vertical-align: top;">
+			</form>
+		</div>
+		<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+		<?php include_once(Root_Path.'/view/navbar.php') ?>
 		</div>
 	</div>
